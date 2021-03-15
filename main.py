dad1efe8978d025d31f92cc0e6e359510e20b30d
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-03-15 12:59:06 -0400

    ok bug fix done

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from tornado.web import Application, url, RequestHandler
 from tornado.ioloop import IOLoop
 
-class HomeHandler(): pass
+class HomeHandler(RequestHandler):
+    def get(self):
+        self.write('pranked')
 
 def prepare_app():
+    print('preparing app')
     return Application([
         url(r'/', HomeHandler)
     ])
 app = prepare_app()
-app.listen(8888)
-IOLoop().current().start()
+print('listening')
+port = 8888
+app.listen(port)
 print('server started')
+print(f'127.0.0.1:{port}')
+IOLoop().current().start()
